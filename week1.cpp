52287e2766428a6288ace6c809cc4216eae48b2a
imgeothegreat <56289649+imgeothegreat@users.noreply.github.com>
2019-10-14 19:39:00 +0800

    Add files via upload

---
+++ week1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	
+	do{
+		cout << "Enter num";
+		cin >> num;
+	}while(num >8 || num <1 || isdigit(num));
+	
+	cout << endl;
+	int space = num -1;
+	
+	for(int j=0;j<num;j++){
+		for(int k=0;k<space;k++){
+			cout << " ";
+		}
+		for(int l=0;l<j+1;l++){
+			cout << '#';
+		}
+		
+		cout << endl;
+		space--;
+	}
+}
